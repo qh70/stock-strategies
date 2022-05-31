f7675fdc2d7dae06b6c53d12b9ec4490749e5929
qh70 <qh7047789@gmail.com>
2022-05-31 23:27:00 +0800

    put redis in ma strategy and corrected sql order

--- app.py
+++ app.py
@@ -187,117 +187,125 @@ def api_getstrategy():
             buffer_date = start_date.replace(year = start_date.year-11)
         buffer_date = datetime.strftime(buffer_date, "%Y%m%d")
         start_date = datetime.strftime(start_date, "%Y%m%d") # 轉回str格式
-        try: 
-            db_connection = pool.get_connection()
-            cursor = db_connection.cursor()
-            cursor.execute("SELECT `最低價`,`日期`,`開盤價`,`收盤價`,`最高價`,`漲跌價差` FROM `all_stocks_and_dates_F_test` WHERE `證券代號` = '"+stock_number+"' AND `日期` BETWEEN  '"+buffer_date+"' AND '"+end_date+"' ORDER BY `日期` DESC;")
-            result_for_ma = cursor.fetchall()
 
-            trade_dates = [] # 所有交易日期
-            above_ma_dates = [] # 列出所有收盤價在均線上方的日期
-            under_ma_dates = [] # 列出所有收盤價在均線下方的日期
-            result_add_ma = [] # 所有最低價、日期、開盤價、收盤價、均價集合
+        if stock_number == "23300": # 從redis拿資料
+            r = redis.Redis(host="myrediscluster.6sqss0.ng.0001.use1.cache.amazonaws.com", port=6379)
+            get_2330 = eval(r.get("2330"))
+            result_for_m = []
+            for i in range(len(get_2330)):
+                if get_2330[i][0] >= buffer_date and get_2330[i][0] <= end_date:
+                    result_for_ma.append(get_2330[i])
+        else: # 從MySQL拿資料
+            try: 
+                db_connection = pool.get_connection()
+                cursor = db_connection.cursor()
+                cursor.execute("SELECT `日期`,`開盤價`,`最高價`,`最低價`,`收盤價`,`漲跌價差` FROM `all_stocks_and_dates_F_test` WHERE `證券代號` = '"+stock_number+"' AND `日期` BETWEEN  '"+buffer_date+"' AND '"+end_date+"' ORDER BY `日期` DESC;")
+                result_for_ma = cursor.fetchall()
+            except mysql.connector.Error as err:
+                print(err, "error msg")
+            finally:
+                db_connection.close()
 
-            for i in range(len(result_for_ma)-how_many_ma+1): # 共有幾天
-                if result_for_ma[i][1] >= start_date:
-                    total = 0
-                    for j in range(i,i+how_many_ma): # 每天的均線
-                        close = result_for_ma[j][3]
-                        total = total+close
-                    result_add_ma.append([result_for_ma[i][0]/100, result_for_ma[i][1], result_for_ma[i][2]/100, result_for_ma[i][3]/100, (math.floor(total/how_many_ma))/100.0, result_for_ma[i][4]/100, result_for_ma[i][5]/100])
-                else:
+        trade_dates = [] # 所有交易日期
+        above_ma_dates = [] # 列出所有收盤價在均線上方的日期
+        under_ma_dates = [] # 列出所有收盤價在均線下方的日期
+        result_add_ma = [] # 所有最低價、日期、開盤價、收盤價、均價集合
+
+        for i in range(len(result_for_ma)-how_many_ma+1): # 共有幾天
+            if result_for_ma[i][0] >= start_date:
+                total = 0
+                for j in range(i,i+how_many_ma): # 每天的均線
+                    close = result_for_ma[j][4]
+                    total = total+close
+                result_add_ma.append([result_for_ma[i][3]/100, result_for_ma[i][0], result_for_ma[i][1]/100, result_for_ma[i][4]/100, (math.floor(total/how_many_ma))/100.0, result_for_ma[i][2]/100, result_for_ma[i][5]/100])
+            else:
+                break
+        result_add_ma.reverse()
+        for k in range(len(result_add_ma)): # 分類站上與跌破均線的日期集合
+            if result_add_ma[k][3] > result_add_ma[k][4]:
+                above_ma_dates.append([result_add_ma[k][1], result_add_ma[k][4], result_add_ma[k][3]])
+            elif result_add_ma[k][3] < result_add_ma[k][4]:
+                under_ma_dates.append([result_add_ma[k][1], result_add_ma[k][4], result_add_ma[k][3]])
+        # 當首日開盤價小於等於均線，找尋第一天收盤價在均線之上的日期
+        if result_add_ma[0][2] <= result_add_ma[0][4]:
+            if len(above_ma_dates) > 0: # 如果有收盤價在均線上方的日期
+                trade_dates.append(["買進", above_ma_dates[0][0], above_ma_dates[0][2]])
+                find_status = "down" # 往跌破均線找
+                for m in range(len(result_add_ma)):
+                    if find_status == "down":
+                        for n in range(len(under_ma_dates)):
+                            if under_ma_dates[n][0] > trade_dates[-1][1]:
+                                trade_dates.append(["賣出", under_ma_dates[n][0], under_ma_dates[n][2]])
+                                del under_ma_dates[:under_ma_dates.index(under_ma_dates[n])+1]
+                                find_status = "up"
+                                break
+                    else:
+                        for p in range(len(above_ma_dates)):
+                            if above_ma_dates[p][0] > trade_dates[-1][1]:
+                                trade_dates.append(["買進", above_ma_dates[p][0], above_ma_dates[p][2]])
+                                del above_ma_dates[:above_ma_dates.index(above_ma_dates[p])+1]
+                                find_status = "down"
+                                break
+            else: # 如果沒有收盤價在均線上方的日期
+                trade_dates = [["沒有買入點", "", ""],["", "", ""]]
+                            
+        else: # 當首日開盤價大於均線，找尋第一天最低價小於等於均價且收盤價大於等於均價的日期
+            for q in range(len(result_add_ma)):
+                if result_add_ma[q][0] <= result_add_ma[q][4] and result_add_ma[q][3] >= result_add_ma[q][4]:
+                    trade_dates.append(["買進", result_add_ma[q][1], result_add_ma[q][3]])
+                    find_status = "down"
                     break
-            result_add_ma.reverse()
-            for k in range(len(result_add_ma)): # 分類站上與跌破均線的日期集合
-                if result_add_ma[k][3] > result_add_ma[k][4]:
-                    above_ma_dates.append([result_add_ma[k][1], result_add_ma[k][4], result_add_ma[k][3]])
-                elif result_add_ma[k][3] < result_add_ma[k][4]:
-                    under_ma_dates.append([result_add_ma[k][1], result_add_ma[k][4], result_add_ma[k][3]])
-            # 當首日開盤價小於等於均線，找尋第一天收盤價在均線之上的日期
-            if result_add_ma[0][2] <= result_add_ma[0][4]:
-                if len(above_ma_dates) > 0: # 如果有收盤價在均線上方的日期
-                    trade_dates.append(["買進", above_ma_dates[0][0], above_ma_dates[0][2]])
-                    find_status = "down" # 往跌破均線找
-                    for m in range(len(result_add_ma)):
-                        if find_status == "down":
-                            for n in range(len(under_ma_dates)):
-                                if under_ma_dates[n][0] > trade_dates[-1][1]:
-                                    trade_dates.append(["賣出", under_ma_dates[n][0], under_ma_dates[n][2]])
-                                    del under_ma_dates[:under_ma_dates.index(under_ma_dates[n])+1]
-                                    find_status = "up"
-                                    break
-                        else:
-                            for p in range(len(above_ma_dates)):
-                                if above_ma_dates[p][0] > trade_dates[-1][1]:
-                                    trade_dates.append(["買進", above_ma_dates[p][0], above_ma_dates[p][2]])
-                                    del above_ma_dates[:above_ma_dates.index(above_ma_dates[p])+1]
-                                    find_status = "down"
-                                    break
-                else: # 如果沒有收盤價在均線上方的日期
-                    trade_dates = [["沒有買入點", "", ""],["", "", ""]]
-                                
-            else: # 當首日開盤價大於均線，找尋第一天最低價小於等於均價且收盤價大於等於均價的日期
-                for q in range(len(result_add_ma)):
-                    if result_add_ma[q][0] <= result_add_ma[q][4] and result_add_ma[q][3] >= result_add_ma[q][4]:
-                        trade_dates.append(["買進", result_add_ma[q][1], result_add_ma[q][3]])
-                        find_status = "down"
-                        break
-                if len(trade_dates) > 0: # 如果有最低價小於等於均價且收盤價大於等於均價的日期
-                    for m in range(len(result_add_ma)):
-                        if find_status == "down":
-                            for n in range(len(under_ma_dates)):
-                                if under_ma_dates[n][0] > trade_dates[-1][1]:
-                                    trade_dates.append(["賣出", under_ma_dates[n][0], under_ma_dates[n][2]])
-                                    del under_ma_dates[:under_ma_dates.index(under_ma_dates[n])+1]
-                                    find_status = "up"
-                                    break
-                        else:
-                            for p in range(len(above_ma_dates)):
-                                if above_ma_dates[p][0] > trade_dates[-1][1]:
-                                    trade_dates.append(["買進", above_ma_dates[p][0], above_ma_dates[p][2]])
-                                    del above_ma_dates[:above_ma_dates.index(above_ma_dates[p])+1]
-                                    find_status = "down"
-                                    break
-                else: # 如果沒有最低價小於等於均價且收盤價大於等於均價的日期
-                    trade_dates = [["沒有買入點", "", ""],["", "", ""]]
-                if len(trade_dates)%2 == 1:
-                    trade_dates.append(["回測最後一天賣出", result_add_ma[-1][1], result_add_ma[-1][3]])
+            if len(trade_dates) > 0: # 如果有最低價小於等於均價且收盤價大於等於均價的日期
+                for m in range(len(result_add_ma)):
+                    if find_status == "down":
+                        for n in range(len(under_ma_dates)):
+                            if under_ma_dates[n][0] > trade_dates[-1][1]:
+                                trade_dates.append(["賣出", under_ma_dates[n][0], under_ma_dates[n][2]])
+                                del under_ma_dates[:under_ma_dates.index(under_ma_dates[n])+1]
+                                find_status = "up"
+                                break
+                    else:
+                        for p in range(len(above_ma_dates)):
+                            if above_ma_dates[p][0] > trade_dates[-1][1]:
+                                trade_dates.append(["買進", above_ma_dates[p][0], above_ma_dates[p][2]])
+                                del above_ma_dates[:above_ma_dates.index(above_ma_dates[p])+1]
+                                find_status = "down"
+                                break
+            else: # 如果沒有最低價小於等於均價且收盤價大於等於均價的日期
+                trade_dates = [["沒有買入點", "", ""],["", "", ""]]
+            if len(trade_dates)%2 == 1:
+                trade_dates.append(["回測最後一天賣出", result_add_ma[-1][1], result_add_ma[-1][3]])
 
-            #================================================================生成圖片需要的資料
+        #================================================================生成圖片需要的資料
 
-            stock_daily_0_to_full=[]
-            for i in range(len(result_add_ma)):
-                # 改date格式
-                date_list = list(result_add_ma[i][1])
-                date_list.insert(4,"-")
-                date_list.insert(7,"-")
-                date = "".join(date_list)
-                # 傳送資料
-                stock_daily = {
-                    "open": result_add_ma[i][2], 
-                    "high": result_add_ma[i][5], 
-                    "close": result_add_ma[i][3], 
-                    "low": result_add_ma[i][0], 
-                    # "volume": result[i][3], 
-                    "price_change": result_add_ma[i][6],
-                    "date": date,
-                    "ma5": result_add_ma[i][4],
-                    # "ma10": result[i][13],
-                    # "ma20": result[i][14]
-                } 
-                stock_daily_0_to_full.append(stock_daily.copy())
-            #================================================================
-        except mysql.connector.Error as err:
-            print(err, "error msg")
-        finally:
-            db_connection.close()
+        stock_daily_0_to_full=[]
+        for i in range(len(result_add_ma)):
+            # 改date格式
+            date_list = list(result_add_ma[i][1])
+            date_list.insert(4,"-")
+            date_list.insert(7,"-")
+            date = "".join(date_list)
+            # 傳送資料
+            stock_daily = {
+                "open": result_add_ma[i][2], 
+                "high": result_add_ma[i][5], 
+                "close": result_add_ma[i][3], 
+                "low": result_add_ma[i][0], 
+                # "volume": result[i][3], 
+                "price_change": result_add_ma[i][6],
+                "date": date,
+                "ma5": result_add_ma[i][4],
+                # "ma10": result[i][13],
+                # "ma20": result[i][14]
+            } 
+            stock_daily_0_to_full.append(stock_daily.copy())
+        #================================================================
+        
     # 算報酬率
     reward = 0
     if trade_dates[0][0] != "沒有買入點":
         for m in range(int(len(trade_dates)/2)):
-            print(trade_dates[m*2-2][2])
             reward = reward+(trade_dates[m*2-1][2]-trade_dates[m*2-2][2])/trade_dates[m*2-2][2]*100
-    print(trade_dates)
     return jsonify({"trade_dates_and_price": trade_dates,"draw_pic_data": stock_daily_0_to_full, "reward": math.floor(reward*100) / 100.0, "stock_name_and_number": stock_number})
     
 app.run(host="0.0.0.0",port=3000)
